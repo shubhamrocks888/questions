3240192050c247ef419e7d725d3d86820e92c13e
unknown <shubhamrocks888@gmail.com>
2020-09-03 15:59:08 +0530

    application programming interface

---
+++ WEB_DEVELOPMENT/API.py
@@ -0,0 +1,21 @@
+What is an API?
+
+Application Program Interface
+
+APIs are a set of functions and procedures that allow for the creation of applications that access data and
+features of other applications, services, or operating systems.
+
+According to Wikipedia, good APIs make it easier to develop a computer program by providing all the building blocks,
+which are then put together by the programmer.”
+
+So, what exactly is an API?
+An application program interface (API) is a toolset that programmers can use to help them create software. Simply put,
+an API specifies how software components should interact.
+
+
+##A real-world example
+A real-world example is buying movie tickets online. You go to the movie site; you enter your movie, name and credit card information,
+and lo-and-behold, you print out your tickets. But what’s going on between entering your information to receiving your ticket? APIs, that’s what!
+
+They are collaborating behind the scenes with other applications. How is this possible you ask? This type of integration is called “seamless”
+because you never have a clue when a software role is passed from one application to another.   
